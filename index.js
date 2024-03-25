dcdb882e17f36663fa26d1142bbc87c17c349f50
Ricky Junaidi <106050611+RickyJ2@users.noreply.github.com>
2024-03-25 16:30:14 +0700

    chg to notifyAGV, simple communicaiton, json format

--- index.js
+++ index.js
@@ -8,29 +8,16 @@ function onSocketError(err) {
 const server = createServer();
 const wss = new WebSocketServer({noServer: true });
 
-let listAGVClient = [];
+let listAGVClient = {};
 let listDashboardClient = [];
-/*
-  Message Format
-    [Code]-[AGVID]-[Data]
-  List code:
-  - 01 : LIDAR Data
-  - 02 : Battery Status
-  - 03 : AGV's speed and orientation
-  - 11 : AGV's goal position
-*/
 wss.on('connection', function connection(ws, request, client) {
   const userId = request.headers['id'];
 
   // Check if the client is AGV or Dashboard
   if(request.url === '/agv'){
-    listAGVClient.push(ws);
-    // Notify dashboard that AGV is online
-    listDashboardClient.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(userId + ' online');
-      }
-    });
+    //add to list of AGV
+    listAGVClient[userId] = ws;
+    notifyDashboard(userId + ' online');
   }else if(request.url === '/dashboard'){
     listDashboardClient.push(ws);
   }
@@ -38,44 +25,53 @@ wss.on('connection', function connection(ws, request, client) {
   ws.on('error', onSocketError);
 
   ws.on('message', function message(data) {
-    let [code, agvId, message] = data.toString().split('-');
-    switch(code){
-      case '01':
-        console.log(`LIDAR Data from AGV ${agvId}: ${message}`);
-        break;
-      case '02':
-        console.log(`Battery Status from AGV ${agvId}: ${message}`);
-        break;
-      case '03':
-        console.log(`Speed and Orientation from AGV ${agvId}: ${message}`);
-        break;
-      case '11':
-        console.log(`Goal Position from AGV ${agvId}: ${message}`);
-        break;
-      default:
-        console.log('Invalid message');
+    if(request.url === '/agv'){
+      console.log(data.toString());
+    }else if(request.url === '/dashboard'){
+      data = JSON.parse(data.toString());
+      notifyAGV(data.cmd, data.id);
     }
   });
 
   ws.on("close", function close() {
-    console.log("disconnected");
     if(request.url === '/agv'){
-      listAGVClient = listAGVClient.filter(function(client) {
-        return client !== ws;
-      });
-      // Notify dashboard that AGV is offline
-      listDashboardClient.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(userId + ' offline');
-        }
-      });
+      delete listAGVClient[userId];
+      notifyDashboard(userId + ' offline');
+      console.log('AGV ' + userId + ' disconnected')
     }else if(request.url === '/dashboard'){
       listDashboardClient = listDashboardClient.filter(function(client) {
         return client !== ws;
       });
+      console.log('Dashboard disconnected')
     }
   });
 });
+
+function notifyAGV(message, agvId){
+  //For targeted AGV id
+  if(agvId != 0){
+    let client = listAGVClient[agvId];
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+    return;
+  }
+  let listAGV = listAGVClient.keys();
+  listAGV.forEach(function each(agvId) {
+    let client = listAGVClient[agvId];
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+}
+
+function notifyDashboard(message){
+  listDashboardClient.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+}
 
 /*
   pass: 1234
@@ -92,6 +88,6 @@ server.on('upgrade', function upgrade(request, socket, head) {
   });
 });
 
-server.listen(8080, "127.0.0.1", function(err){
+server.listen(8080, function(err){
   console.log(err, server.address())
 });
