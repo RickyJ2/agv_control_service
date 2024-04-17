f393cef5498323a7e406314a8471a48d55b1d890
Ricky Junaidi <106050611+RickyJ2@users.noreply.github.com>
2024-04-17 15:09:55 +0700

    agv class

---
+++ class/agv.js
@@ -0,0 +1,48 @@
+class AGV {
+    constructor(id) {
+        this.id = id;
+        this.container = false;
+        this.collision = false;
+        this.power = 100;
+        this.orientation = 0;
+        this.acceleration = { x: 0, y: 0 };
+        this.localMap = null;
+
+        this.position = { x: 0, y: 0 };
+        this.ws = null;
+
+        this.listGoalPoint = [];
+        this.listPath = [];
+    }
+    updateState(data){
+        this.container = data.container;
+        this.collision = data.collision;
+        this.power = data.power;
+        this.orientation = data.orientation;
+        this.acceleration = data.acceleration;
+        this.localMap = data.localMap;
+    }
+    setWs(ws) {
+        this.ws = ws;
+    }
+    setPosition(x, y) {
+        this.position = { x, y };
+    }
+    getState(){
+        return {
+            id: this.id,
+            isOnline: this.isOnline(),
+            container: this.container,
+            collision: this.collision,
+            power: this.power,
+            orientation: this.orientation,
+            acceleration: this.acceleration,
+            position: this.position
+        }
+    }
+    isOnline(){
+        return this.ws != null;
+    }
+}
+
+export default AGV;
--- index.js
+++ index.js
@@ -1,51 +1,16 @@
 import { createServer } from 'http';
 import WebSocket, { WebSocketServer } from 'ws';
+import AGV from './class/agv.js';
+
+let listAGVClient = {
+  '1': AGV(1),
+  '2': AGV(2),
+};
+let listDashboardClient = [];
 
 function onSocketError(err) {
   console.error(err);
 }
-
-const server = createServer();
-const wss = new WebSocketServer({noServer: true });
-
-let listAGVClient = {};
-let listDashboardClient = [];
-wss.on('connection', function connection(ws, request, client) {
-  const userId = request.headers['id'];
-
-  // Check if the client is AGV or Dashboard
-  if(request.url === '/agv'){
-    //add to list of AGV
-    listAGVClient[userId] = ws;
-    notifyDashboard(userId + ' online');
-  }else if(request.url === '/dashboard'){
-    listDashboardClient.push(ws);
-  }
-
-  ws.on('error', onSocketError);
-
-  ws.on('message', function message(data) {
-    if(request.url === '/agv'){
-      console.log(data.toString());
-    }else if(request.url === '/dashboard'){
-      data = JSON.parse(data.toString());
-      notifyAGV(data.cmd, data.id);
-    }
-  });
-
-  ws.on("close", function close() {
-    if(request.url === '/agv'){
-      delete listAGVClient[userId];
-      notifyDashboard(userId + ' offline');
-      console.log('AGV ' + userId + ' disconnected')
-    }else if(request.url === '/dashboard'){
-      listDashboardClient = listDashboardClient.filter(function(client) {
-        return client !== ws;
-      });
-      console.log('Dashboard disconnected')
-    }
-  });
-});
 
 function notifyAGV(message, agvId){
   //For targeted AGV id
@@ -56,6 +21,7 @@ function notifyAGV(message, agvId){
     }
     return;
   }
+  //broadcast to all AGV
   let listAGV = listAGVClient.keys();
   listAGV.forEach(function each(agvId) {
     let client = listAGVClient[agvId];
@@ -66,6 +32,7 @@ function notifyAGV(message, agvId){
 }
 
 function notifyDashboard(message){
+  //broadcast to all Dashboard
   listDashboardClient.forEach(function each(client) {
     if (client.readyState === WebSocket.OPEN) {
       client.send(message);
@@ -73,20 +40,65 @@ function notifyDashboard(message){
   });
 }
 
-/*
-  pass: 1234
-*/
-server.on('upgrade', function upgrade(request, socket, head) {
-  if(request.headers['websocketpass']?.toString() != '1234'){
-    socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
-    socket.destroy();
-    return;
+const server = createServer();
+const wss = new WebSocketServer({noServer: true });
+
+wss.on('connection', function connection(ws, request, client) {
+  const userId = request.headers['id'];
+  // Check if the client is AGV or Dashboard
+  if(request.url === '/agv'){
+    listAGVClient[userId].setWs(ws);
+    console.log('AGV ' + userId + ' connected');
+  }else if(request.url === '/dashboard'){
+    listDashboardClient.push(ws);
+    console.log('Dashboard connected');
   }
-  wss.handleUpgrade(request, socket, head, function (ws) {
-    wss.emit('connection', ws, request);
-    console.log("connected")
+
+  ws.on('error', onSocketError);
+
+  ws.on('message', function message(data) {
+    if(request.url === '/agv'){
+      data = JSON.parse(data.toString());
+      listAGVClient[userId].updateState(data);
+      msg = {
+        type: 'update',
+        data: listAGVClient[userId].getState()
+      }
+      notifyDashboard(JSON.stringify(msg));
+    }else if(request.url === '/dashboard'){
+      data = JSON.parse(data.toString());
+      //generatePath for AGV
+      notifyAGV(data.cmd, data.id);
+    }
+  });
+
+  ws.on("close", function close() {
+    if(request.url === '/agv'){
+      listAGVClient[userId].setWs(null);
+      console.log('AGV ' + userId + ' disconnected');
+    }else if(request.url === '/dashboard'){
+      listDashboardClient = listDashboardClient.filter(function(client) {
+        return client !== ws;
+      });
+      console.log('Dashboard disconnected')
+    }
   });
 });
+/*
+  comment server on upgrade if dont want to use password
+  pass: 1234
+*/
+// server.on('upgrade', function upgrade(request, socket, head) {
+//   if(request.headers['websocketpass']?.toString() != '1234'){
+//     socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+//     socket.destroy();
+//     return;
+//   }
+//   wss.handleUpgrade(request, socket, head, function (ws) {
+//     wss.emit('connection', ws, request);
+//     console.log("connected")
+//   });
+// });
 
 server.listen(8080, function(err){
   console.log(err, server.address())
